5fbb8ef719a71287d33158c3d2aaa5db9dafb60c
lisandradecassia <lisandradecassia@gmail.com>
2022-08-19 20:10:17 -0300

    feat: connecting route from footer.module.css

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import S from "./Footer.module.css";
 
 const Footer = () => {
   return (
-    <footer>
-      <p>Testando suas habilidades em HTML, CSS e JS.</p>
-      <p>Linx Impulse</p>
-      <p>2019</p>
+    <footer className={S.footer}>
+      <p className={S.p}>Testando suas habilidades em HTML, CSS e JS.</p>
+      <p className={S.p}>Linx Impulse</p>
+      <p className={S.p}>2019</p>
     </footer>
   );
 };
