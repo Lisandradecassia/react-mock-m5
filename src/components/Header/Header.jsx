a45be359e056521c9fe735311794c4dfa7453997
lisandradecassia <lisandradecassia@gmail.com>
2022-08-20 10:45:11 -0300

    feat: including header information

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,9 @@ import S from "./Header.module.css";
 
 const Header = () => {
   return (
-    <header>
+    <header className={S.header}>
+      <h2 className={S.h2}>uma seleção de produtos</h2>
+      <h1 className={S.h1}>especial para você</h1>
       <p className={S.p}>
         Todos os produtos desta lista foram selecionados a partir da sua
         navegação. Aproveite!
