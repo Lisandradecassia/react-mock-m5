2c2390f4d9d7e53020dfe3e7ce9cd7820b1d2cbb
lisandradecassia <lisandradecassia@gmail.com>
2022-08-20 12:17:31 -0300

    feat: adding route to button

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
+import Button from './components/Button/Button'
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Footer/>
+      <Button/>
     </div>
   );
 }
